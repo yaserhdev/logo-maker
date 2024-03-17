d3f1cf8dc477c2a355e502368487d057f66ec084
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-17 17:46:23 -0400

    Created index.js file (imported inquirer and fs, and created logo class with constructor function for logo input values) and added .gitignore file

---
+++ index.js
@@ -0,0 +1,13 @@
+// Import inquirer and fs
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Define logo class
+class Logo {
+    constructor() {
+        this.text = '';
+        this.textColor = '';
+        this.shape = '';
+        this.shapeColor = '';
+    }
+};

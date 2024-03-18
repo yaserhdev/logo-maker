9f37463bfc36bd883c1e072cb926a2bc0c66f530
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-18 00:18:41 -0400

    Added code for generating all logos with all shapes

--- index.js
+++ index.js
@@ -1,90 +1,82 @@
 // Import inquirer and fs
 const inquirer = require('inquirer');
 const fs = require('fs');
+// Import shape classes
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
+const Square = require('./lib/square.js');
 
-// Define logo class
-class Logo {
-    constructor() {
-        this.text = '';
-        this.textColor = '';
-        this.shape = '';
-        this.shapeColor = '';
-    }
-    // Prompt user input for  text, text color, shape, and shape color
-    async logoPrompt() {
-        const questions = [
-            {
-                type: 'input',
-                name: 'text',
-                message: 'Enter between 1-3 characters for your logo text:',
-                validate: (value) => {
-                    if (value.length <= 3) {
-                        return true;
-                    } else {
-                        return 'Text must be between 1-3 characters'
-                    }
+// Prompt user input for  text, text color, shape, and shape color
+async function logoPrompt() {
+    const questions = [
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter between 1-3 characters for your logo text:',
+            validate: (value) => {
+                if (value.length <= 3) {
+                    return true;
+                } else {
+                    return 'Text must be between 1-3 characters'
                 }
-            },
-            {
-                type: 'input',
-                name: 'text-color',
-                message: 'Enter the desired text color:',
-                validate: (value) => {
-                    if (value.length > 0) {
-                        return true;
-                    } else {
-                        return 'Please provide color'
-                    }
-                }
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                message: 'Please select a shape:',
-                choices: ['Circle', 'Triangle', 'Square']
-            },
-            {
-                type: 'input',
-                name: 'shape-color',
-                message: 'Enter the desired shape color:',
-                validate: (value) => {
-                    if (value.length > 0) {
-                        return true;
-                    } else {
-                        return 'Please provide a color'
-                    }
-                }
-            },
-        ]
-        return await inquirer.prompt(questions);
-    };
-    // Function to generate SVG markup
-    generateSVG() {
-        this.textColor = this.answers[`text-color`];
-        this.shapeColor = this.answers['shape-color'];
-        return
-        `<svg width="200" height="200">
-            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="60" fill="${this.textColor}">${this.text}</text>
-            ${this.shape === 'Circle' ? `<circle cx="100" cy="100" r="50" fill="${this.shapeColor}" />` : ''}
-            ${this.shape === 'Triangle' ? `<polygon points="50,15 100,100 0,100" fill="${this.shapeColor}" />` : ''}
-            ${this.shape === 'Square' ? `<rect x="50" y="50" width="100" height="100" fill="${this.shapeColor}" />` : ''}
-        </svg>`;
-    }
-    // Function to write SVG file
-    writeSVG() {
-        const markup = this.generateSVG();
-        fs.writeToFile('logo', markup, (err) => {
-            if (err) {
-                console.error('Error writing SVG file:', err);
-            } else {
-                console.log(`SVG file saved as ${'logo'}`)
             }
-        })
-    }
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the desired text color:',
+            validate: (value) => {
+                if (value.length > 0) {
+                    return true;
+                } else {
+                    return 'Please provide color'
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Please select a shape:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the desired shape color:',
+            validate: (value) => {
+                if (value.length > 0) {
+                    return true;
+                } else {
+                    return 'Please provide a color'
+                }
+            }
+        },
+    ]
+    return await inquirer.prompt(questions);
 };
 
-// Create new instance of logo class and call method to get user input
-const output = new Logo();
-output.logoPrompt().then((answers) => {
-    console.log(answers);
+// Call logo prompt and generate logo based on user input
+logoPrompt().then((answers) => {
+    if (answers.shape === 'Circle') {
+        const circle = new Circle(answers.text, answers.textColor, answers.shapeColor);
+        try {
+            fs.writeFileSync('./examples/circle.svg', circle.render());
+        } catch (err) {
+            console.error('Error writing SVG file:', err);
+        }
+    } else if  (answers.shape === 'Triangle') {
+        const triangle = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+        try {
+            fs.writeFileSync('./examples/triangle.svg', triangle.render());
+        } catch (err) {
+            console.log('Error writing SVG file:', err)
+        }
+    } else {
+        const square = new Square(answers.text, answers.textColor, answers.shapeColor);
+        try {
+            fs.writeFileSync('./examples/square.svg', square.render());
+        } catch (err) {
+            console.log('Error writing SVG file:', err)
+        }
+    }
 });
